6943539645459cc4d64df1b5ddbbe6f0b1c13ef0
RamSrivastava01 <89699290+RamSrivastava01@users.noreply.github.com>
2026-06-10 20:02:42 +0530

    fixed prettier issue of indenting the files

--- Routes/filesRoutes.js
+++ Routes/filesRoutes.js
@@ -3,17 +3,15 @@ import { createWriteStream } from "fs";
 import { mkdir, readdir, rename, rm, stat, writeFile } from "fs/promises";
 import cors from "cors";
 import path, { dirname, join } from "path";
-import crypto from 'crypto'
-import filesData from '../filesDb.json' with {type: 'json'}
-import directoriesData from '../directoriesDB.json' with {type: "json"}
-
-
+import crypto from "crypto";
+import filesData from "../filesDb.json" with { type: "json" };
+import directoriesData from "../directoriesDB.json" with { type: "json" };
 
 const router = express.Router();
 // File Post Route
 router.post("/{:parentDirId}", (req, res) => {
-   const parentDirId = req.params.parentDirId || req.user.rootDirId
-   const filename = req.headers.filename || "untitled"
+   const parentDirId = req.params.parentDirId || req.user.rootDirId;
+   const filename = req.headers.filename || "untitled";
    const id = crypto.randomUUID();
    const extension = path.extname(filename);
    const fullFileName = `${id}${extension}`;
@@ -25,17 +23,24 @@ router.post("/{:parentDirId}", (req, res) => {
          extension,
          name: filename,
          parentDirId,
-      })
-      const parentDirData = directoriesData.find((directoryData) => directoryData.id === parentDirId)
-      parentDirData.files.push(id)
+      });
+      const parentDirData = directoriesData.find(
+         (directoryData) => directoryData.id === parentDirId,
+      );
+      parentDirData.files.push(id);
       try {
-         await writeFile('./filesDB.json', JSON.stringify(filesData))
-         await writeFile('./directoriesDB.json', JSON.stringify(directoriesData))
+         await writeFile("./filesDB.json", JSON.stringify(filesData));
+         await writeFile(
+            "./directoriesDB.json",
+            JSON.stringify(directoriesData),
+         );
          return res.status(201).json({ message: "File Uploaded" });
       } catch (error) {
-         // if we do next here then the error is catched by the global middleware. 
+         // if we do next here then the error is catched by the global middleware.
          // Global middleware catches all the request sent by any route. if next() is called
-         res.status(404).json({ message: "Something went wrong while creating the File" })
+         res.status(404).json({
+            message: "Something went wrong while creating the File",
+         });
       }
    });
 });
@@ -43,43 +48,49 @@ router.post("/{:parentDirId}", (req, res) => {
 //File Get Route
 
 router.get("/:id", (req, res) => {
-   console.log(req.url)
-   const { id } = req.params
-   const fileData = filesData.find((file) => file.id === id)
-
+   console.log(req.url);
+   const { id } = req.params;
+   const fileData = filesData.find((file) => file.id === id);
+   const parentDir = directoriesData.find(
+      (dir) => dir.id === fileData.parentDirId,
+   );
+   console.log({ parentDir });
+   if (parentDir.userId !== req.user.id) {
+      return res
+         .status(401)
+         .json({ error: "You don't have access to this file" });
+   }
    if (!fileData) {
-      return res.status(404).json({ message: `${id} does not exist` })
+      return res.status(404).json({ message: `${id} does not exist` });
    }
 
    if (req.query.action === "download") {
       res.set("Content-Disposition", `attachment; filename=${fileData.name}`);
    }
-   res.sendFile(`${process.cwd()}/storage/${id}${fileData.extension}`, (err) => {
-      // console.log(err);
-      if (!res.headersSent && err) {
-         return res.status(404).json({ error: "File not found!" });
-      }
-   });
+   res.sendFile(
+      `${process.cwd()}/storage/${id}${fileData.extension}`,
+      (err) => {
+         // console.log(err);
+         if (!res.headersSent && err) {
+            return res.status(404).json({ error: "File not found!" });
+         }
+      },
+   );
 });
 
 // File Patch Route
 
 router.patch("/:id", async (req, res) => {
-
-
-
    const { id } = req.params;
-   console.log({ body: req.body })
-   const fileData = filesData.find((file) => file.id === id)
+   console.log({ body: req.body });
+   const fileData = filesData.find((file) => file.id === id);
    fileData.name = req.body.newFilename;
    try {
-      await writeFile("./filesDB.json", JSON.stringify(filesData))
+      await writeFile("./filesDB.json", JSON.stringify(filesData));
       return res.json({
          message: "Renamed Successfully",
       });
-
    } catch (err) {
-
       console.error(err);
 
       return res.status(500).json({
@@ -91,30 +102,31 @@ router.patch("/:id", async (req, res) => {
 // File Delete Route
 
 router.delete("/:id", async (req, res) => {
-
    const { id } = req.params;
-   const fileIndex = filesData.findIndex((file) => file.id === id)
+   const fileIndex = filesData.findIndex((file) => file.id === id);
    if (fileIndex == -1) {
-      return res.status(404).json({ message: "File is not available" })
+      return res.status(404).json({ message: "File is not available" });
    }
-   const fileData = filesData[fileIndex]
+   const fileData = filesData[fileIndex];
 
    try {
       await rm(`./storage/${id}${fileData.extension}`, { recursive: true });
-      filesData.splice(fileIndex, 1)
-      const parentDirData = directoriesData.find((directoryData) => directoryData.id === fileData.parentDirId)
+      filesData.splice(fileIndex, 1);
+      const parentDirData = directoriesData.find(
+         (directoryData) => directoryData.id === fileData.parentDirId,
+      );
 
-      parentDirData.files = parentDirData.files.filter((fileId) => fileId != id)
+      parentDirData.files = parentDirData.files.filter(
+         (fileId) => fileId != id,
+      );
 
-      await writeFile("./filesDB.json", JSON.stringify(filesData))
-      await writeFile("./directoriesDB.json", JSON.stringify(directoriesData))
+      await writeFile("./filesDB.json", JSON.stringify(filesData));
+      await writeFile("./directoriesDB.json", JSON.stringify(directoriesData));
 
       return res.json({
          message: "File Deleted Successfully",
       });
-
    } catch (err) {
-
       console.error(err);
 
       return res.status(404).json({
@@ -123,4 +135,4 @@ router.delete("/:id", async (req, res) => {
    }
 });
 
-export default router
+export default router;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,28 +1,25 @@
 import express from "express";
 import cors from "cors";
-import crypto from 'crypto'
-import directoriesData from '../directoriesDB.json' with {type: "json"}
-import usersData from "../usersDB.json" with {type: "json"}
+import crypto from "crypto";
+import directoriesData from "../directoriesDB.json" with { type: "json" };
+import usersData from "../usersDB.json" with { type: "json" };
 import { writeFile } from "fs/promises";
-
-
-
+import CheckAuth from "../auth.js";
 
 const router = express.Router();
 
 //use cookie parser for parsing the request from the browser
 // use command npm i cookie-
 
-
 // File Post Route
 
 router.post("/register", async (req, res, next) => {
-   const { name, email, password } = req.body
-   console.log(name)
-   const foundUser = usersData.find((user) => user.email == email)
+   const { name, email, password } = req.body;
+   console.log(name);
+   const foundUser = usersData.find((user) => user.email == email);
 
    if (foundUser) {
-      return res.status(409).json({ error: "User already exists" })
+      return res.status(409).json({ error: "User already exists" });
    }
 
    const dirId = crypto.randomUUID();
@@ -33,38 +30,56 @@ router.post("/register", async (req, res, next) => {
       userId,
       parentDirId: null,
       files: [],
-      directories: []
-
-   })
+      directories: [],
+   });
    usersData.push({
-      id: userId, name, email, password,
-      rootDirId: dirId
-   })
+      id: userId,
+      name,
+      email,
+      password,
+      rootDirId: dirId,
+   });
 
    try {
-      await writeFile('./directoriesDB.json', JSON.stringify(directoriesData))
-      await writeFile('./usersDB.json', JSON.stringify(usersData))
-      res.status(201).json({ message: `User created with ID ${userId}` })
+      await writeFile("./directoriesDB.json", JSON.stringify(directoriesData));
+      await writeFile("./usersDB.json", JSON.stringify(usersData));
+      res.status(201).json({ message: `User created with ID ${userId}` });
    } catch (error) {
-      next(error)
+      next(error);
    }
+});
 
-})
+// GET user route
+
+router.get("/", CheckAuth, async (req, res) => {
+   return res.status(200).json({
+      name: req.user.name,
+      email: req.user.email,
+   });
+});
 
 router.post("/login", async (req, res) => {
    const { email, password } = req.body;
    const user = usersData.find((user) => user.email == email);
-   if (!user || (user.password != password)) {
-      return res.status(404).json({ error: "Invalid credentials" })
+   if (!user || user.password != password) {
+      return res.status(404).json({ error: "Invalid credentials" });
    }
 
    res.cookie("uid", user.id, {
       httpOnly: true,
-      maxAge: 60 * 1000 * 60 * 24 * 7
-   })
+      maxAge: 60 * 1000 * 60 * 24 * 7,
+   });
 
+   res.json({ message: "logged in" });
+});
 
-   res.json({ message: "logged in" })
-})
+router.post("/logout", (req, res) => {
+   res.cookie("uid", "", {
+      maxAge: 0,
+   });
+
+   //we can also use clearCookie() --> This will clear the cookie from the client
+   res.status(200).json({ message: "User logged out !" });
+});
 
 export default router;
